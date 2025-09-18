bff4b6dd7e66cef8723de31d03a5ad0c8a7a816d
pedguedes090 <noradavid319j23jaco@gmail.com>
2025-09-18 21:50:53 +0700

    upload

--- ss31/client/src/router/Router.tsx
+++ ss31/client/src/router/Router.tsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import PostList from "../components/PostList";
 export const router = createBrowserRouter([
     {
-        path: "/",
+        path: "/list-post",
         element: <PostList />
     }
 ])
